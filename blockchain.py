989b9ec832df3d54051a1c763ac1a8f121b0b162
Pedro <pedrolealwerneckdiniz@gmail.com>
2024-09-26 21:44:50 -0300

    Terceiro commit - Mais umas mexidas ai cz.

--- blockchain.py
+++ blockchain.py
@@ -1,23 +1,36 @@
-def Blockchain(object):
+import hashlib
+import json
+
+from time import time 
+from uuid import uuid4
+
+
+class Blockchain(object): 
     def __init__(self):
         self.chain = []
         self.current_transactions = []
     
     def new_block(self):
-        # cria um novo bloco e adiciona ele pra corrente
+        """
+        Cria um novo bloco na BlockChain
+        :param proof: <int> A prova dada pela prova de trabalho do algoritimo
+        :param previous_hash: (Opicional) <str> Hash do bloco anterior
+        :return: <dict> Novo bloco.
+        """
+        
         block = {
-            'index': 1
-            'timestamp': 1506057125.900785
-            'transactions':´[
-                {
-                    'sender': "8527147fe1f5426f9dd545de4b27ee00"
-                    'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f"
-                    'amount': 5,
-                }
-            ], 
-            'proof': 324984774000
-            'previus_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            
         }
+        # Reseta a linha atual de transações
+        self.current_transactions: []
+        
+        self.chain.append(block)
+        return block
     
     def new_transaction(self):
         # adiciona uma nova transação para a lista de transações
@@ -36,14 +49,50 @@ def Blockchain(object):
         
         return self.last.block['index'] + 1
     
+    def proof_of_work(self, last_proof):
+        """
+        Prova simples de algoritimo de trabalho
+         - encontre um numero p' que tanto quanto hash(pp') contenha 4 zeros no começo,
+         onde p é o antigo p'
+         - p é a antiga prova, e p' é a nova prova
+         :param last_proof: <int>
+         :return: <int>
+        """
+        
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        
+        return proof
     
     @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Válida a prova: Hash tem 4 zeros iniciais?(last_proof, proof)
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+    
+    
+    
     def hash(block):
         # da um hash em um bloco
-        pass
+        """
+        Cria um SHA-256 hash de um bloco
+        :param block: <dict> Bloco
+        :return: <str>
+        """
+        
+        # tem que prestar atenção para que o diciionario seja chamado, se nao vao ser hashes inconsistentes 
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
     
     @property
     def last_block(self):
         # retorna o ultimo bloco na corrente
-        pass
+        return self.chain[-1]
     
